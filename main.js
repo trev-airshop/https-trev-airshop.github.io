2de0d0edfff04e04b100538e4762eb66dfdcd295
trev-airshop <134011997+trev-airshop@users.noreply.github.com>
2023-11-13 20:57:23 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// main.js
 import { createInput, resetForm } from './formUtils.js';
 import { updateTable, addDeleteButtonToRow } from './tableUtils.js';
 import ProductManager from './productManager.js';
@@ -8,15 +7,33 @@ document.addEventListener("DOMContentLoaded", function () {
   const getElement = (id) => document.getElementById(id);
   const colorVariantsContainer = getElement("colorInputs");
   const addColorVariantButton = getElement("addColorVariant");
-  // ... other getElement calls
+  const sizeVariantsContainer = getElement("sizeInputs");
+  const addSizeVariantButton = getElement("addSizeVariant");
+  const submissionTable = getElement("submissionTable");
+  const submissionBody = getElement("submissionBody");
 
-  const productManager = new ProductManager(updateTable);
+  const productManager = new ProductManager(updateTable, submissionBody);
 
   addColorVariantButton.addEventListener("click", function () {
     const colorInput = createInput("text", "color", "Color");
     colorVariantsContainer.appendChild(colorInput);
   });
 
+  addSizeVariantButton.addEventListener("click", function () {
+    // ... Similar logic for size variants
+  });
+
+  getElement("generateSKUs").addEventListener("click", function () {
+    productManager.generateSKUs();
+  });
+
+  getElement("submitProduct").addEventListener("click", function () {
+    // Logic to gather data from form and call productManager.addProduct()
+  });
+
+  getElement("downloadCSV").addEventListener("click", function () {
+    downloadCSV(productManager.products);
+  });
+
   // ... other event listeners
-  // You will need to modify event listeners to use methods from productManager and other imported utilities.
 });
