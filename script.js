0c9f749819b199a9713e112eb507f8d3a52eb0c9
trev-airshop <134011997+trev-airshop@users.noreply.github.com>
2023-12-21 14:37:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -312,16 +312,16 @@ addBlankRowButton.addEventListener("click", function() {
 
   function downloadShopifyCSV() {
     // Headers from the Shopify template
-    const headers = ["Handle", "Title", "Body (HTML)", "Vendor", "Product Category",	"Type",	"Tags",	"Published",	"Option1 Name",	"Option1 Value",	"Option2 Name",	"Option2 Value",	"Option3 Name",	"Option3 Value",	"Variant SKU",	"Variant Grams",	"Variant Inventory Tracker",	"Variant Inventory Qty",	"Variant Inventory Policy",	"Variant Fulfillment Service",	"Variant Price",	"Variant Compare At Price",	"Variant Requires Shipping",	"Variant Taxable",	"Variant Barcode",	"Image Src",	"Image Position",	"Image Alt Text",	"Gift Card",	"SEO Title",	"SEO Description",	"Google Shopping / Google Product Category",	"Google Shopping / Gender",	"Google Shopping / Age Group",	"Google Shopping / MPN",	"Google Shopping / AdWords Grouping",	"Google Shopping / AdWords Labels",	"Google Shopping / Condition",	"Google Shopping / Custom Product",	"Google Shopping / Custom Label 0",	"Google Shopping / Custom Label 1",	"Google Shopping / Custom Label 2",	"Google Shopping / Custom Label 3",	"Google Shopping / Custom Label 4",	"Variant Image",	"Variant Weight Unit",	"Variant Tax Code",	"Cost per item",	"Price / International",	"Compare At Price / International",	"Status"]; // List all headers here
+    const headers = ["Handle", "Title", "Body (HTML)", "Vendor", "Product Category", "Type",	"Tags",	"Published",	"Option1 Name",	"Option1 Value",	"Option2 Name",	"Option2 Value",	"Option3 Name",	"Option3 Value",	"Variant SKU",	"Variant Grams",	"Variant Inventory Tracker",	"Variant Inventory Qty",	"Variant Inventory Policy",	"Variant Fulfillment Service",	"Variant Price",	"Variant Compare At Price",	"Variant Requires Shipping",	"Variant Taxable",	"Variant Barcode",	"Image Src",	"Image Position",	"Image Alt Text",	"Gift Card",	"SEO Title",	"SEO Description",	"Google Shopping / Google Product Category",	"Google Shopping / Gender",	"Google Shopping / Age Group",	"Google Shopping / MPN",	"Google Shopping / AdWords Grouping",	"Google Shopping / AdWords Labels",	"Google Shopping / Condition",	"Google Shopping / Custom Product",	"Google Shopping / Custom Label 0",	"Google Shopping / Custom Label 1",	"Google Shopping / Custom Label 2",	"Google Shopping / Custom Label 3",	"Google Shopping / Custom Label 4",	"Variant Image",	"Variant Weight Unit",	"Variant Tax Code",	"Cost per item",	"Price / International",	"Compare At Price / International",	"Status"]; // List all headers here
 
     // Hardcoded and dynamic values from the second row of the template
     const defaultValues = {
         "Handle": "", // Leave blank or add logic if needed
-        "Title": () => product.productName,
+        "Title": (product) => product.productName,
         "Body (HTML)": "", // Leave blank or add logic if needed
-        "Vendor": () => product.vendor,
+        "Vendor": (product) => product.vendor,
         "Product Category": "",
-        "Type": () => product.productType,
+        "Type": (product) => product.productType,
         "Tags": "",
         "Published": "TRUE", 
         "Option1 Name": "",
@@ -330,17 +330,17 @@ addBlankRowButton.addEventListener("click", function() {
         "Option2 Value": "",
         "Option3 Name": "",
         "Option3 Value": "",
-        "Variant SKU": () => product.sku,
-        "Variant Grams": () => product.size,
+        "Variant SKU": (product) => product.sku,
+        "Variant Grams": (product) => product.size,
         "Variant Inventory Tracker": "Shopify",
         "Variant Inventory Qty": "",
         "Variant Inventory Policy": "deny", 
         "Variant Fulfillment Service": "manual", 
-        "Variant Price": () => product.price,
+        "Variant Price": (product) => product.price,
         "Variant Compare At Price": "",
         "Variant Requires Shipping": "TRUE", 
         "Variant Taxable": "TRUE", 
-        "Variant Barcode": () => product.barcode,
+        "Variant Barcode": (product) => product.barcode,
         "Image Src": "",
         "Image Position": "",
         "Image Alt Text": "",
@@ -363,7 +363,7 @@ addBlankRowButton.addEventListener("click", function() {
         "Variant Image": "",
         "Variant Weight Unit": "",
         "Variant Tax Code": "",
-        "Cost per item": () => product.cost,
+        "Cost per item": (product) => product.cost,
         "Price / International": "",
         "Compare At Price / International": "",
         "Status": "Draft",
