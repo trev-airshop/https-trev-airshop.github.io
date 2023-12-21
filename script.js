3e2b8fd9e98d87bba124d465db5bf1afcd897dd9
trev-airshop <134011997+trev-airshop@users.noreply.github.com>
2023-12-21 15:59:22 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,24 @@ document.addEventListener("DOMContentLoaded", function () {
     colorVariantsContainer.appendChild(colorInput);
   });
 
+  // PapaParse code to parse the CSV file
+    let typeToDetailsMap = {};
+
+    Papa.parse('path/to/your/csv/file.csv', {
+        download: true,
+        header: true,
+        complete: function(results) {
+            results.data.forEach(row => {
+                const type = row['Type'];
+                const category = row['Category'];
+                const tags = row['Tags'];
+                typeToDetailsMap[type] = { category, tags };
+            });
+
+            // You can now use typeToDetailsMap in your application
+        }
+    });
+
   addSizeVariantButton.addEventListener("click", function () {
     const sizeInput = createInput("text", "size", "Size Variant");
     const priceInput = createInput("text", "price", "Price");
@@ -310,9 +328,11 @@ addBlankRowButton.addEventListener("click", function() {
  
   // NEW CODE START SHOPIFY CSV
 
+  
+
   function downloadShopifyCSV() {
     // Headers from the Shopify template
-    const headers = ["Handle", "Title", "Body (HTML)", "Vendor", "Product Category", "Type",	"Tags",	"Published",	"Option1 Name",	"Option1 Value",	"Option2 Name",	"Option2 Value",	"Option3 Name",	"Option3 Value",	"Variant SKU",	"Variant Grams",	"Variant Inventory Tracker",	"Variant Inventory Qty",	"Variant Inventory Policy",	"Variant Fulfillment Service",	"Variant Price",	"Variant Compare At Price",	"Variant Requires Shipping",	"Variant Taxable",	"Variant Barcode",	"Image Src",	"Image Position",	"Image Alt Text",	"Gift Card",	"SEO Title",	"SEO Description",	"Google Shopping / Google Product Category",	"Google Shopping / Gender",	"Google Shopping / Age Group",	"Google Shopping / MPN",	"Google Shopping / AdWords Grouping",	"Google Shopping / AdWords Labels",	"Google Shopping / Condition",	"Google Shopping / Custom Product",	"Google Shopping / Custom Label 0",	"Google Shopping / Custom Label 1",	"Google Shopping / Custom Label 2",	"Google Shopping / Custom Label 3",	"Google Shopping / Custom Label 4",	"Variant Image",	"Variant Weight Unit",	"Variant Tax Code",	"Cost per item",	"Price / International",	"Compare At Price / International",	"Status"]; // List all headers here
+    const headers = ["Handle", "Title", "Body (HTML)", "Vendor", "Product Category", "Type", "Tags",	"Published",	"Option1 Name",	"Option1 Value",	"Option2 Name",	"Option2 Value",	"Option3 Name",	"Option3 Value",	"Variant SKU",	"Variant Grams",	"Variant Inventory Tracker",	"Variant Inventory Qty",	"Variant Inventory Policy",	"Variant Fulfillment Service",	"Variant Price",	"Variant Compare At Price",	"Variant Requires Shipping",	"Variant Taxable",	"Variant Barcode",	"Image Src",	"Image Position",	"Image Alt Text",	"Gift Card",	"SEO Title",	"SEO Description",	"Google Shopping / Google Product Category",	"Google Shopping / Gender",	"Google Shopping / Age Group",	"Google Shopping / MPN",	"Google Shopping / AdWords Grouping",	"Google Shopping / AdWords Labels",	"Google Shopping / Condition",	"Google Shopping / Custom Product",	"Google Shopping / Custom Label 0",	"Google Shopping / Custom Label 1",	"Google Shopping / Custom Label 2",	"Google Shopping / Custom Label 3",	"Google Shopping / Custom Label 4",	"Variant Image",	"Variant Weight Unit",	"Variant Tax Code",	"Cost per item",	"Price / International",	"Compare At Price / International",	"Status"]; // List all headers here
 
     // Hardcoded and dynamic values from the second row of the template
     const defaultValues = {
